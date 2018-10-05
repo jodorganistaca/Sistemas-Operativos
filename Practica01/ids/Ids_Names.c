6097a132e06940fb02a32bbef8ad941531a84682
Jose Daniel Organista <jodorganistaca@unal.edu.co>
2018-10-05 10:20:25 -0500

    Add files via upload

--- Practica01/ids/Ids_Names.c
+++ Practica01/ids/Ids_Names.c
@@ -6,7 +6,6 @@
 int hash_function(char a[32]){
     int hash = 0;
     for (int i = 0; i < 32; i++) {
-        //printf("%i\t",a[i]);
         if(a[i]>=65&&a[i]<=90){
             a[i]+=32;
         }
@@ -27,7 +26,6 @@ int main(){
     i =0;
     while(fgets(line, sizeof(line)-1,fileR)){
         strtok(line, "\n");
-        /*printf("%s\n",line);*/
         strcpy(names[i],line);
         printf("%i\n",hash_function(names[i]));
         i+=1;
@@ -36,9 +34,7 @@ int main(){
     FILE *fileW = fopen("namesIds.txt","w");
     for(i = 0; i<1000;i++){
         int id = hash_function(names[i]);
-        char *idN[10] = ;
-        fwrite(id,sizeof(int),1,fileW);
-        //fwrite('\n',sizeof(char),1,fileW);
+        fprintf(fileW, "Name %s, Id: %d\n", names[i], id);
     }
     fclose(fileW);
 }
