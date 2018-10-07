3c28fd1eefccfaa6e176bc5e9dc8dfd74a37a271
Jose Daniel Organista <jodorganistaca@unal.edu.co>
2018-10-06 19:00:35 -0500

    Add files via upload

--- Practica01/ids/Ids_Names.c
+++ Practica01/ids/Ids_Names.c
@@ -17,24 +17,37 @@ int hash_function(char a[32]){
 
 int main(){
     int i;
-    FILE *fileR = fopen("names.txt","r");
-    char line[256];
-    char *names[1000];
-    for(i = 0; i<1000;i++){
+    FILE *fileR = fopen("names.txt","r");//Abre el archivo en modo lectura
+    char line[256]; //buffer de tamaño 256 para leer la linea de el archivo
+    char *names[1000]; //arreglo de tamaño 1000 de punteros para los nombres
+    char *freeId[10];//arreglo de tamaño 10 de punteros para los ids libres
+    int arrId[1010];//arreglo para saber que pos estan libres
+    for(i = 0; i<1000;i++){//Reservo espacio para cada puntero
         names[i] = malloc( 32 * sizeof(char));
+        arrId[i] = 0;
+        //freeId[i] = malloc( 10 * sizeof(char));
     }
-    i =0;
-    while(fgets(line, sizeof(line)-1,fileR)){
-        strtok(line, "\n");
-        strcpy(names[i],line);
-        printf("%i\n",hash_function(names[i]));
-        i+=1;
+	for(;i<1010;i++){
+		arrId[i] = 0;
+	}
+    i =0;//reinicio el contador
+    while(fgets(line, sizeof(line)-1,fileR)){//Mientras lea el archivo de nombres
+        strtok(line, "\n"); //Los separa hasta que haya un salto de linea
+        strcpy(names[i],line);//copia lo que esta en la linea y lo copia en el arreglo de punteros
+        //printf("%i\n",hash_function(names[i]));//muestra en consola los id de cada nombre
+        arrId[hash_function(names[i])]++;//aumenta el contador en la pos del hash
+        i+=1;//aumenta contador
     }
-    fclose(fileR);
-    FILE *fileW = fopen("namesIds.txt","w");
+    fclose(fileR);//Cierra archivo de lectura
+    FILE *fileW = fopen("namesIds.txt","w");//Abrimos un archivo de escritura para los nombres y ids
+    FILE *fileW2 = fopen("freeIds.txt","w");//Abrimos un archivo de escritura solo los ids libres
     for(i = 0; i<1000;i++){
         int id = hash_function(names[i]);
-        fprintf(fileW, "Name %s, Id: %d\n", names[i], id);
+        fprintf(fileW, " %d %s \n", id, names[i]);
+        if(arrId[id]<=0){//Si el arrId esta en 0 significa que no esta escrito una mascota en esta posicion
+			fprintf(fileW2,"%i\t",id);
+        }
     }
     fclose(fileW);
+	fclose(fileW2);
 }
